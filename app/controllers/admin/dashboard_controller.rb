19607da2fb84a1bd1bfb63fc128611bca5df7b64
JamesArmstrong92 <jarmst23@gmail.com>
2021-04-29 11:41:27 -0400

    Added admin dash counter

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,10 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @total_products = Product.count
+    @total_categories = Category.count
   end
-
-  private
   
+  private
+
   http_basic_authenticate_with name: 'Jungle', password: 'book'
 end
