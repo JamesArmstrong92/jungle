174b68be40965f4f02102a8d43c5734985db3ebb
JamesArmstrong92 <jarmst23@gmail.com>
2021-04-30 09:46:29 -0400

    Added new user auth controllers, updated db, updated top nav

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,16 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
+
+  # Routes used to log in and out
+  get  '/login'  => 'sessions#new'
+  post '/login'  => 'sessions#create'
+  get  'logout'  => 'sessions#destory'
+  
+  get  '/signup' => 'users#new'
+  post '/users'  => 'users#create'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
