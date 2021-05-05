feb0285ae5a414345511c622f6533600da7133b5
JamesArmstrong92 <jarmst23@gmail.com>
2021-05-05 13:35:02 -0400

    Fixed formatting error

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Vistor vists the home page, Then is brought to product page if and when they click on a product", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "The user is able to see the products page" do
+    # ACT
+    visit root_path
+
+    first('a.btn-default').click
+
+    # DEBUG
+    save_screenshot
+
+    # VERIFY
+    expect(page).to have_css 'section.products-show', count: 1
+    save_and_open_screenshot 
+  end
 end
